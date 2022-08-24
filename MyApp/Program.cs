9eb03985ec6e8c7444a8ae2ca3fe709b09ef2f9a
Patan Moulali <patan.moulali@mphasis.com>
2022-08-24 15:51:09 +0530

    Add project files.

---
+++ MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReuseableCodeLibrary;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass.PrintString();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ReuseableCodeLibrary/Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ReuseableCodeLibrary
+{
+    public class SampleClass
+    {
+        public static void PrintString()
+        {
+            Console.WriteLine("printing the string");
+        }
+    }
+}
